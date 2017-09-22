e6f9c9f4ecad8f82ed0004c8d6f481c10e66a8cc
Leon <leon.kim.0509@gmail.com>
2017-09-22 17:02:55 -0400

    Apple Picker
    I think I sent it incorrectly lat time

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+	void Awake() {
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore")) { 
+			score = PlayerPrefs.GetInt ("ApplePickerHighScore");
+		}
+		PlayerPrefs.SetInt ("ApplePickerHighScore", score); 
+	}
+	void Update () {
+		GUIText gt = this.GetComponent<GUIText>();
+		gt.text = "High Score: "+score;
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore")) {
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+}
